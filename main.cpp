576afb46b54ab79026eab15172ff13d9a306e2be
exp <geekaman22july@gmail.com>
2021-05-25 19:49:15 +0530

    modified CMakeLists.txt and commented main

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@
 int main(int argc, char* argv[]){ 
         std::cout << "Welcome to code!! " << std::endl;
 
-        glfwInit();
+        glfwInit(); // initializing the glew
 
             glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
             glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
             glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+            /**
+             * @brief Here we are basically initializing the window 
+             * and creating a window object.
+             */
             GLFWwindow* window = glfwCreateWindow(800, 800, "OPENGL PROJECT", NULL, NULL);
             if (window == NULL){
                 std::cout << "Failed to create window!!" << std::endl;
@@ -23,9 +27,9 @@ int main(int argc, char* argv[]){
                 gladLoadGL();
                 glViewport(0, 0, 800, 800);
 
-                glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT);
-                glfwSwapBuffers(window);
+                glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // this is the color stored in the back
+                glClear(GL_COLOR_BUFFER_BIT); // color stored in the foregoround.
+                glfwSwapBuffers(window); // swapping the buffer so as after each rendering our screen get clear
 
                 while (!glfwWindowShouldClose(window)){
                     glfwPollEvents();
