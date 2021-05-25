50501079dc2bd05b6043dd0eed153b832305e5ed
exp <geekaman22july@gmail.com>
2021-05-25 19:38:43 +0530

    few changes updated

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(int argc, char* argv[]){ 
@@ -18,7 +19,14 @@ int main(int argc, char* argv[]){
             }
 
             glfwMakeContextCurrent(window);
-            
+
+                gladLoadGL();
+                glViewport(0, 0, 800, 800);
+
+                glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
+                glfwSwapBuffers(window);
+
                 while (!glfwWindowShouldClose(window)){
                     glfwPollEvents();
                 }
